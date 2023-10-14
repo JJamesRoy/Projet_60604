a6735ba0a16ad4e3d7e9d903be3ecd9a7c7f56b7
JRCharlemagne <james22god@live.ca>
2023-10-14 19:24:08 -0400

    add data and stuff

--- mergedata.R
+++ mergedata.R
@@ -6,11 +6,13 @@ dat_mid = read.csv("2021_stations.csv")
 dat_join = inner_join(dat_merge, dat_mid, by=c("STARTSTATIONNAME"="name"))
 dat_join = dat_join %>% distinct(STARTSTATIONNAME, .keep_all = TRUE)
 dat_join = dat_join %>% select(c("STARTSTATIONNAME","STARTSTATIONARRONDISSEMENT", "pk"))
-dat_join = inner_join(dat_join,dat, by=c("pk"="station"))
+dat_join = right_join(dat_join,dat, by=c("pk"="station"))
 dat_full= dat_join %>% 
   rename(
     name = STARTSTATIONNAME,
     station = pk,
     arrond = STARTSTATIONARRONDISSEMENT
   )
+dat_full <- dat_full %>%
+  mutate(arrond = ifelse(is.na(arrond), "autres", arrond))
 write.csv(dat_full, "bixifull.csv")
